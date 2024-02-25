a8ee1340fe5b0b81db0251988a9e500e7f58e9dc
maggiemay-afk <hermsmk@gmail.com>
2024-02-24 19:19:06 -0500

    import text encoder / decoder in app.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { TextEncoder, TextDecoder } from 'util';
+
+Object.assign(global, { TextDecoder, TextEncoder });
 
 test('renders learn react link', () => {
   render(<App />);
