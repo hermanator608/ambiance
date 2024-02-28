0c962a465224e93752ec86fb1c56f1b80e60f2c9
maggiemay-afk <hermsmk@gmail.com>
2024-02-28 10:42:10 -0500

    Still getting errors

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,18 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './AuthProvider';
-import { Routes } from "react-router-dom";
+//import { AuthProvider } from './AuthProvider';
+
 
 test('renders learn react link', () => {
   render(
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <App />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   );
   const mainEl = screen.getByTestId('main')
   expect(mainEl).toBeInTheDocument();
