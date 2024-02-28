7a9b4bae9edbf7bc9853844986c037cd5f3bc061
maggiemay-afk <hermsmk@gmail.com>
2024-02-28 10:26:24 -0500

    Add browser router to test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
   const mainEl = screen.getByTestId('main')
   expect(mainEl).toBeInTheDocument();
 });
