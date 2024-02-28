bdb91cfec3e759331f6dd66ca88743bca4d1dc37
maggiemay-afk <hermsmk@gmail.com>
2024-02-28 10:46:06 -0500

    add back the AuthProvider to test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-//import { AuthProvider } from './AuthProvider';
+import { AuthProvider } from './AuthProvider';
 
 
 test('renders learn react link', () => {
   render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   );
   const mainEl = screen.getByTestId('main')
   expect(mainEl).toBeInTheDocument();
