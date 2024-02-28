71ee068bb350c4b9803ad5533487a5197200c57f
maggiemay-afk <hermsmk@gmail.com>
2024-02-28 10:57:05 -0500

    Trying MemoryRouter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import { AuthProvider } from './AuthProvider';
 
 
 test('renders learn react link', () => {
   render(
     <AuthProvider>
-      <BrowserRouter>
+      <MemoryRouter>
         <App />
-      </BrowserRouter>
+      </MemoryRouter>
     </AuthProvider>
   );
   const mainEl = screen.getByTestId('main')
