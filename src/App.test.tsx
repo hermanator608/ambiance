d1caf9ae8d510bfc8db116b01a9083df2e002571
Brandon Herman <bherman@twilio.com>
2024-02-28 12:27:45 -0500

    Test mocks and new tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,16 +3,40 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './AuthProvider';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
+jest.mock('firebase/auth');
 
-test('renders learn react link', () => {
+test('renders App on the main ambaince page with logged in user', () => {
+  (getAuth as jest.Mock).mockReturnValue({
+      currentUser: {
+        email: "testUser@gmail.com"
+      }
+  });
+
+  (onAuthStateChanged as jest.Mock).mockReturnValue(jest.fn());
+
   render(
-    <AuthProvider>
-      <BrowserRouter>
+    <BrowserRouter>
+      <AuthProvider>
         <App />
-      </BrowserRouter>
-    </AuthProvider>
+      </AuthProvider>
+    </BrowserRouter>
   );
   const mainEl = screen.getByTestId('main')
   expect(mainEl).toBeInTheDocument();
+});
+
+test('renders Main ambaince page with no user', () => {
+
+});
+
+// Need to setup browserrouter to go to /admin
+test('renders Admin page with logged in user', () => {
+
+});
+
+// Need to setup browserrouter to go to /admin
+test('renders Login page when going to admin page with no user', () => {
+
 });
